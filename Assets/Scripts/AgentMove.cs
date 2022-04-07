b810f749c84bca15ad6acf5b1206280a03e085c4
harrisongiger <harrisongiger21@gmail.com>
2022-04-07 00:33:30 -0700

    lotta stuff

--- Assets/Scripts/AgentMove.cs
+++ Assets/Scripts/AgentMove.cs
@@ -5,15 +5,129 @@ using UnityEngine.AI;
 
 public class AgentMove : MonoBehaviour
 {
+    NavMeshAgent agent;
 
-    
+    [SerializeField]
+    float senserange = 6f;
+    [SerializeField]
+    float energy = 10f;
+    Vector3 startingpos;
+    [SerializeField]
+    int foodcounter;
+    [SerializeField]
+    float time = 10f;
+    bool timeup = false;
+    public float startingspeed;
+    public float newspeed;
+
     void Start()
     {
-        
+        agent = this.GetComponent<NavMeshAgent>();
+        startingspeed = Random.Range(2.5f, 10f);
+        agent.speed = startingspeed;
+        agent.stoppingDistance = 2;
+        startingpos = agent.transform.position;
+        Wander();
     }
 
     void Update()
     {
+        energy -= (senserange * agent.speed * Time.deltaTime) / 30f;
+        time -= Time.deltaTime;
+        if (!agent.pathPending)
+        {
+            if (agent.remainingDistance <= agent.stoppingDistance)
+            {
+                if (agent.pathStatus == NavMeshPathStatus.PathComplete)
+                {
+                    Wander();
+                }
+            }
+        }
+        if (energy > 0f)
+        {
+            InvokeRepeating("FoodSearch", 0f, 0.1f);
+        }
+        else
+        {
+            if (timeup == false)
+            {
+                agent.SetDestination(agent.transform.position);
+            }
+            if (time <= 0f)
+            {
+                agent.SetDestination(startingpos);
+                timeup = true;
+            } 
+            if (time < -15f)
+            {
+                timeup = false;
+                agent.speed = 5f;
+                energy = 10f;
+                time = 10f;
+                Sex();
+            }
+        }
         
     }
+
+    void FoodSearch()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(gameObject.transform.position, senserange);
+        GameObject closest = null;
+        float closestDist = Mathf.Infinity;
+        bool found = false;
+
+        foreach (var hitCollider in hitColliders)
+        {
+            if (hitCollider.gameObject.CompareTag("Food"))
+            {
+                float dist = Vector3.Distance(transform.position, hitCollider.gameObject.transform.position);
+                if (dist < closestDist)
+                {
+                    closestDist = dist;
+                    closest = hitCollider.gameObject;
+                    found = true;
+                }
+
+            }
+        }
+        if (found)
+        {
+            CancelInvoke();
+            agent.SetDestination(closest.transform.position);
+        }
+    }
+
+    void Wander()
+    {
+        agent.SetDestination(new Vector3(Random.Range(-10f, 10f), 0, Random.Range(-10f, 10f)));
+    }
+
+    public void Sex()
+    {
+        if (foodcounter == 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (foodcounter >= 1)
+        {
+            foodcounter = 0;
+            
+            GameObject obj = Instantiate(gameObject, gameObject.transform.position, Quaternion.identity);
+            
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Food") && energy > 0f)
+        {
+            Destroy(other.gameObject);
+            foodcounter++;
+        }
+        
+
+    }
 }
+
--- Assets/Scripts/PlayerSpawner.cs
+++ Assets/Scripts/PlayerSpawner.cs
@@ -4,28 +4,67 @@ using UnityEngine;
 
 public class PlayerSpawner : MonoBehaviour
 {
+    public static PlayerSpawner shared;
+
     public GameObject Agent;
     public int AgentCount;
-
+    public int foodCount;
+    public GameObject Food;
+    public GameObject[] AllAgents;
+    float energy = 10f;
+    [SerializeField]
+    int day = 0;
+    
     void Start()
     {
+        shared = this;
+        AllAgents = new GameObject[AgentCount];
         for (var i = 0; i < AgentCount; i++) {
         float randomnum = Mathf.Floor(Random.Range(0f, 4f));
-        switch (randomnum) {
-            case 0: 
-            Instantiate(Agent, new Vector3(55f, 1f, Random.Range(-50f, 50f)), Quaternion.identity);
-            break;
-            case 1: 
-            Instantiate(Agent, new Vector3(-55f, 1f, Random.Range(-50f, 50f)), Quaternion.identity);
-            break;
-            case 2: 
-            Instantiate(Agent, new Vector3(Random.Range(-50f, 50f), 1f, 55f), Quaternion.identity);
-            break;
-            case 3: 
-            Instantiate(Agent, new Vector3(Random.Range(-50f, 50f), 1f, -55f), Quaternion.identity);
-            break;
+            if (randomnum == 0)
+            {
+                GameObject obj = Instantiate(Agent, new Vector3(55f, 1f, Random.Range(-50f, 50f)), Quaternion.identity);
+      //          AllAgents[i] = (obj);
+                  
+            }
+            else if (randomnum == 1)
+            {
+
+                GameObject obj = Instantiate(Agent, new Vector3(-55f, 1f, Random.Range(-50f, 50f)), Quaternion.identity);
+      //          AllAgents[i] = (obj);
+            }
+            else if (randomnum == 2)
+            {
+                GameObject obj = Instantiate(Agent, new Vector3(Random.Range(-50f, 50f), 1f, 55f), Quaternion.identity);
+      //          AllAgents[i] = (obj);
+            }
+            else if (randomnum == 3)
+            {
+                GameObject obj = Instantiate(Agent, new Vector3(Random.Range(-50f, 50f), 1f, -55f), Quaternion.identity);
+      //          AllAgents[i] = (obj);
+            }
         }
 
+        FoodSpawn();
+        
+    }
+
+    private void FoodSpawn()
+    {
+        for (var i = 0; i < foodCount; i++)
+        {
+            Instantiate(Food, new Vector3(Random.Range(-49f, 49f), 1, Random.Range(-49f, 49f)), Quaternion.identity);
+        }
+    }
+
+    private void Update()
+    {
+        energy -= Time.deltaTime;
+        if (energy < -15f)
+        {
+            day++;
+            energy = 10f;
+            FoodSpawn();
         }
     }
 }
